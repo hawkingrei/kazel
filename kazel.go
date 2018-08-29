c7819b992262bcd120b9211479620ead8869f87c
Wangweizhen <hawking.rei@gmail.com>
2018-08-29 19:46:52 +0800

    feat: improve golang/protobuf

--- kazel.go
+++ kazel.go
@@ -956,9 +956,12 @@ func walk(root string, walkFn filepath.WalkFunc) error {
 func depMapping(dep []string) []string {
 	result := []string{}
 	mapping := map[string]string{
-		"//vendor/github.com/golang/protobuf/proto:go_default_library":         "@com_github_golang_protobuf//proto:go_default_library",
-		"//vendor/github.com/golang/protobuf/ptypes/any:go_default_library":    "@io_bazel_rules_go//proto/wkt:any_go_proto",
-		"//vendor/github.com/golang/protobuf/jsonpb:go_default_library":        "@com_github_golang_protobuf//jsonpb:go_default_library",
+		"//vendor/github.com/golang/protobuf/proto:go_default_library":                    "@com_github_golang_protobuf//proto:go_default_library",
+		"//vendor/github.com/golang/protobuf/ptypes/any:go_default_library":               "@io_bazel_rules_go//proto/wkt:any_go_proto",
+		"//vendor/github.com/golang/protobuf/jsonpb:go_default_library":                   "@com_github_golang_protobuf//jsonpb:go_default_library",
+		"//vendor/github.com/golang/protobuf/protoc-gen-go/plugin:go_default_library":     "@com_github_golang_protobuf//protoc-gen-go/plugin:go_default_library",
+		"//vendor/github.com/golang/protobuf/protoc-gen-go/descriptor:go_default_library": "@com_github_golang_protobuf//protoc-gen-go/descriptor:go_default_library",
+
 		"//vendor/github.com/gogo/protobuf/gogoproto:go_default_library":       "@com_github_gogo_protobuf//gogoproto:go_default_library",
 		"//vendor/github.com/gogo/protobuf/proto:go_default_library":           "@com_github_gogo_protobuf//proto:go_default_library",
 		"//vendor/github.com/gogo/protobuf/protoc-gen-gogo:go_default_library": "@com_github_gogo_protobuf//protoc-gen-gogo:go_default_library",
